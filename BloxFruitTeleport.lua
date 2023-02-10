399c9f279fb91d9ec8a3b22900be9b7e63a75245
NatNyt <70100268+NatNyt@users.noreply.github.com>
2023-02-10 23:30:19 +0700

    Update BloxFruitTeleport.lua

--- BloxFruitTeleport.lua
+++ BloxFruitTeleport.lua
@@ -21,11 +21,19 @@ if BF then
     if type(args[1]) ~= "string" and type(args[2]) ~= "number" then return; end
     if game.PlaceId ~= args[2] then 
         game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(JoinServer[args[2]])
+        game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+            if State == Enum.TeleportState.Started then
+                if syn then 
+                    syn.queue_on_teleport("loadstring(game:HttpGet'https://raw.githubusercontent.com/NatNyt/MyScript/main/BloxFruitTeleport.lua'){" .. '"'.. args[1] .. '"' .. args[2] .. "}")   
+                end
+                if queue_on_teleport then 
+                    queue_on_teleport("loadstring(game:HttpGet'https://raw.githubusercontent.com/NatNyt/MyScript/main/BloxFruitTeleport.lua'){" .. '"'.. args[1] .. '"' .. args[2] .. "}")    
+                end
+            end
+        end)
         return; 
     end
-    if queue_on_teleport then 
-        queue_on_teleport()    
-    end
+    game:GetService("ReplicatedStorage").__ServerBrowser:InvokeServer("teleport",args[1])
 else 
     return;
 end
