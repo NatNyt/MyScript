e1f5c1da943eeea4031b2210ef04ca26e69c196c
NatNyt <70100268+NatNyt@users.noreply.github.com>
2023-03-06 19:59:42 +0700

    Update BloxFruitTeleport.lua

--- BloxFruitTeleport.lua
+++ BloxFruitTeleport.lua
@@ -1,42 +1,30 @@
-(function (...) 
-    if game.PlaceId == 2753915549 or  game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+local BF = false
+if game.PlaceId == 2753915549 or  game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
         BF = true
     end
-    if not BF then return; end
+if not BF then return; end
 
-    repeat wait() until game:IsLoaded()
+local JoinServer = (function () 
+	return {
+		[2753915549] = "TravelMain",
+		[4442272183] = "TravelDressrosa",
+		[7449423635] = "TravelZou"
+	}
+end)();
 
-    local JoinServer = (function () 
-            return {
-                [2753915549] = "TravelMain",
-                [4442272183] = "TravelDressrosa",
-                [7449423635] = "TravelZou"
-            }
-    end)();
-
-
-    if BF then 
-        local args = {
-                    
-        }
-        if type(args[1]) ~= "string" and type(args[2]) ~= "number" then return; end
-        if game.PlaceId ~= args[2] then 
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(JoinServer[args[2]])
-            game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
-                if State == Enum.TeleportState.Started then
-                    if syn then 
-                        syn.queue_on_teleport("loadstring(game:HttpGet'https://raw.githubusercontent.com/NatNyt/MyScript/main/BloxFruitTeleport.lua'){" .. '"'.. args[1] .. '"' .. args[2] .. "}")   
-                    end
-                    if queue_on_teleport then 
-                        queue_on_teleport("loadstring(game:HttpGet'https://raw.githubusercontent.com/NatNyt/MyScript/main/BloxFruitTeleport.lua'){" .. '"'.. args[1] .. '"' .. args[2] .. "}")    
-                    end
-                end
-            end)
-            return; 
-        end
-        game:GetService("ReplicatedStorage").__ServerBrowser:InvokeServer("teleport",args[1])
-    else 
-        return;
-    end
-    
-end)(...)
+if BF then 
+	local args = ({...})
+	if type(args[1]) ~= "string" and type(args[2]) ~= "number" then return; end
+	if game.PlaceId ~= args[2] then 
+		game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+			if syn then 
+				syn.queue_on_teleport("loadstring(game:HttpGet'https://raw.githubusercontent.com/NatNyt/MyScript/main/BloxFruitTeleport.lua'){" .. '"'.. args[1] .. '"' .. args[2] .. "}")   
+			end
+			if queue_on_teleport then 
+				queue_on_teleport("loadstring(game:HttpGet'https://raw.githubusercontent.com/NatNyt/MyScript/main/BloxFruitTeleport.lua'){" .. '"'.. args[1] .. '"' .. args[2] .. "}")    
+			end
+		end)
+		game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(JoinServer[args[2]])
+	end
+	game:GetService("ReplicatedStorage").__ServerBrowser:InvokeServer("teleport",args[1])
+end
