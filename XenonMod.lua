4c90edeb1a8b5c0c5406a07692680b1af7d60a68
NatNyt <70100268+NatNyt@users.noreply.github.com>
2023-05-27 01:41:18 +0700

    Update XenonMod.lua

--- XenonMod.lua
+++ XenonMod.lua
@@ -1,2 +1,25 @@
+repeat wait() until game:IsLoaded()
+
 local pbag = ({...})[1]
-print(pbag)
+local args = ""
+for i, v in pairs(pbag) do 
+	if i == 1 then 
+		args = args .. tostring(v) 
+	else
+		args = args  .. ',' .. tostring(v) 
+	end
+end
+
+local queue_teleport = (function() 
+    if typeof(queue_on_teleport) == "function" then
+        return queue_on_teleport
+    end
+    if typeof(syn) == "table" then 
+        return syn.queue_on_teleport
+    end
+end)()
+if typeof(queue_teleport) == "function" then 
+     queue_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua')){" .. args .. "}")
+end
+
+loadstring("loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua')){" .. args .. "}")()
